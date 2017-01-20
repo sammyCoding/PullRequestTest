938434ade89f803397a9e143315a807def36562a
amaruk <amaruk@163.com>
2017-01-20 11:53:02 +0800

    Update print string to indicate modification from master

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 if __name__ == "__main__":
-	print "Hello from branchForRebase."
+	print "Hello fram master again."
 	print "Hello from branchForMerge."
+	print "Hello from branchForRebase."
 	exit(0)
 
