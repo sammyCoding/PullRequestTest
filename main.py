6a0e8b29823d7aa42fc5b2efaa8609446051439b
amaruk <amaruk@163.com>
2017-01-21 11:24:38 +0800

    Update from branchRebase again

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ if __name__ == "__main__":
 	print "Hello fram master again and again."
 	print "Hello from branchForMerge."
 	print "Hello from branchForRebase."
+	print "Hello fram branchRebase again."
 	exit(0)
 
